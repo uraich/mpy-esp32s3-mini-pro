c4dfce72efbbbc321bb93d69da83b0e84958e947
Uli Raich <uli.raich@gmail.com>
2026-02-12 20:19:34 +0100

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This shell scripts sets up the picoweb server to run the Hello World
+# WEB server. It uses picoweb to provide the Hello World WEB page.
+# Demo program for the course on the Internet of Things (IoT) at the
+# University of Cape Coast (Ghana)
+# Copyright (c) U. Raich April 2020
+# This program is released under GPL
+
+echo "Setting up the hardware file for the esp32s3_mini_pro"
+dirs="$(ampy ls)"
+echo $dirs
+
+#check if /lib already exists
+
+if [[ $dirs == *"/lib"* ]]
+then
+    echo "/lib directory already exists"
+    echo "The following modules have been uploaded to /lib:"
+    modules="$(ampy ls /lib)"
+    for i in $modules ; do
+	echo ${i#"/lib/"}
+	done    
+else
+    echo "Creating /lib directory"
+    ampy mkdir /lib
+fi
+
+echo ""
+echo "Uploading hw_esp32s3_mini_pro.py"
+ampy put hw_esp32s3_mini_pro.py /lib/hw_esp32s3_mini_pro.py
+
