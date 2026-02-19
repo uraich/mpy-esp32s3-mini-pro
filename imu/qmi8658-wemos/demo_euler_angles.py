8604912fd7e490565d35ddcc88e6f4332861c462
Uli Raich <uli.raich@gmail.com>
2026-02-19 20:26:06 +0100

    added imu code

---
+++ imu/qmi8658-wemos/demo_euler_angles.py
@@ -0,0 +1,40 @@
+# qmi8658_demo.py: a test program based on the wemos qmi8658 driver
+# This reads the euler angles from the qmi8658 driver
+# Copyright (c) U.Raich, 19. Feb.2026
+#
+# Released under the MIT license
+
+from time import sleep_ms
+from machine import I2C
+from qmi8658 import QMI8658
+
+# try to import the hardware definition file
+import sys
+try:
+    from hw_esp32s3_mini_pro import *
+except:
+    print("Please make sure hw_esp32s3_mini_pro.py has been uploaded to /lib")
+    sys.exit()
+
+i2c = I2C(1,scl=SCL,sda=SDA)
+qmi8658 = QMI8658(i2c)
+
+# the driver has no method to find out if new data are available
+# I read every 500 ms
+print("acc x,y,z")
+try:
+    for i in range(1000):
+        # the driver returns a tuple of x,y,z values
+        gyro_data = qmi8658.read_gyro()
+        acc_data  = qmi8658.read_accel()
+        pitch,roll,yaw = qmi8658.get_euler_angles(gyro_data[0],
+                                                  gyro_data[1],
+                                                  gyro_data[2],
+                                                  acc_data[0],
+                                                  acc_data[1],
+                                                  acc_data[2])
+        
+        print("{:5.3f},{:5.3f},{:5.3f}".format(pitch,roll,yaw))
+        sleep_ms(100)
+except KeyboardInterrupt:
+    pass
